72f7e29c6af8f2084eca1eaa8c0d4e6cd4a812c6
Redinkton <104201122+Redinkton@users.noreply.github.com>
2022-10-10 16:09:59 +0300

    Add files via upload

---
+++ Migrations/20221005141023_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTank.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tanks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    MinVolume = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxVolume = table.Column<int>(type: "INTEGER", nullable: false),
+                    CurrentVolume = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tanks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tanks");
+        }
+    }
+}
