3fa9a6fcb593cc67500e618374ae17a8b399630e
Redinkton <104201122+Redinkton@users.noreply.github.com>
2022-10-10 15:58:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using MyTank.Data;
+using MyTank.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSqlite<TankContext>("Data Source=MyTank.db");
+
+builder.Services.AddScoped<TankService>();
+//builder.Services.AddHostedService<TimedHostedService>();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.CreateDbIfNotExists();
+
+app.Run();
